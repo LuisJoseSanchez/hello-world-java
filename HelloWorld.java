1d9a3137029d47e1fc9e442cc129704399c69a85
root <root@ip-172-31-26-238.ec2.internal>
2022-05-04 13:01:18 +0000

    added some changes in print message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-     System.out.println("Hello Devops");
+     System.out.println("Hello Devop session");
 
   }
 }
