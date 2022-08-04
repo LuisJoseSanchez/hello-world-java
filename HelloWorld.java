687a6f3e169a80a816ba3389230e3383952315de
theja <gitadmin@gmail.com>
2022-08-04 12:02:33 +0000

    Updated hello-world-java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world!");	 
+    System.out.println("Hello world!");	
+    System.out.println("Hello world!");	 
+    System.out.println("Hello world!");	 
+    System.out.println("Hello world!");	 
+    System.out.println("Hello world!");	 
   }
 }
