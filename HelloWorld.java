604df606c8efafb5a2c4868757d643e34c40e470
adigopi49 <94618109+adigopi49@users.noreply.github.com>
2023-09-17 15:02:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
   public static void main(String[] args) {
 
     System.out.println("Hello world!");
-    System.out.println("nice to meet you gopi");
+    System.out.println("nice to you gopi");
+    
        
   }
 }
