d16fbb7cf88059fe0912dbbc1c0ad75890527d73
dzavalapalafox <d.zavala.palafox@gmail.com>
2023-11-06 21:32:38 -0500

    GitHub Assignment - Daniel Zavala-Palafox
    This is an exploration of GitHub and I cloned a simple repository I found online. I added the print statement "This is Daniel exploring Git/GitHub and I am making changes to this repository"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!")
+	  
+    System.out.println("Hello world!");
+    
+    System.out.print("This is Daniel exploring Git/GitHub and I am making changes to this repository");  
+    
+  
   }
 }
